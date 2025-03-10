7d52863f768c82766b1800cd749aff2c46906fd7
dev-vivekkumarverma <vivekkumarverma332@gmail.com>
2025-03-10 10:42:40 +0530

    added code change

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello, World! this is hi from CICD DOCKER")
+    return HttpResponse("Hello, World! this is hi CICD DOCKER")
