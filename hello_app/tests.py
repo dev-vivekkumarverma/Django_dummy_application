45d61f9c892d82107a478f77891c21c7bcefc602
Vivek Kumar Verma <70999057+dev-vivekkumarverma@users.noreply.github.com>
2025-03-11 11:21:34 +0530

    Update tests.py

--- hello_app/tests.py
+++ hello_app/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+class HelloTest(TestCase):
+    def test_hello_view(self):
+        response = self.client.get(reverse('hello_world'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello World")
